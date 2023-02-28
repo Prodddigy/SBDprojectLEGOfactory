19d398b25d94e046fc5b4560d52300aea0fe369b
Damian Czarnecki <70945042+Prodddigy@users.noreply.github.com>
2023-02-28 17:53:13 +0100

    complete

---
+++ LegoFactory_createPL.sql
@@ -0,0 +1,237 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2023-01-11 19:00:02.103
+
+-- tables
+-- Table: Author
+CREATE TABLE Author (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    Surname varchar2(40)  NOT NULL,
+    DOB date  NOT NULL,
+    CONSTRAINT Author_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Author_CompleteSet
+CREATE TABLE Author_CompleteSet (
+    Author_id number(4,0)  NOT NULL,
+    CompleteSet_id number(4,0)  NOT NULL,
+    CONSTRAINT Author_CompleteSet_pk PRIMARY KEY (Author_id,CompleteSet_id)
+) ;
+
+-- Table: Box
+CREATE TABLE Box (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    Capacity number(4,0)  NOT NULL,
+    CompleteSet_id number(4,0)  NOT NULL,
+    CONSTRAINT Box_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Brick
+CREATE TABLE Brick (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    electric smallint  NOT NULL,
+    Material_id number(4,0)  NOT NULL,
+    CONSTRAINT Brick_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Brick_Color
+CREATE TABLE Brick_Color (
+    Color_id number(4,0)  NOT NULL,
+    Brick_id number(4,0)  NOT NULL,
+    CONSTRAINT Brick_Color_pk PRIMARY KEY (Color_id,Brick_id)
+) ;
+
+-- Table: BuildingDifficulty
+CREATE TABLE BuildingDifficulty (
+    id number(4,0)  NOT NULL,
+    CeilingAge number(3,0)  NOT NULL,
+    GroundAge number(3,0)  NOT NULL,
+    CONSTRAINT BuildingDifficulty_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Color
+CREATE TABLE Color (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    RGBcode varchar2(20)  NOT NULL,
+    GlowInDark smallint  NOT NULL,
+    CONSTRAINT Color_pk PRIMARY KEY (id)
+) ;
+
+-- Table: CompleteSet
+CREATE TABLE CompleteSet (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    Theme_id number(4,0)  NOT NULL,
+    BuildingDifficulty_id number(4,0)  NOT NULL,
+    price number(4,2)   NULL,
+    DiscontinueDate_id number(4,0)   NULL,
+    CONSTRAINT CompleteSet_pk PRIMARY KEY (id)
+) ;
+
+-- Table: CompleteSet_Brick
+CREATE TABLE CompleteSet_Brick (
+    CompleteSet_id number(4,0)  NOT NULL,
+    Brick_id number(4,0)  NOT NULL,
+    quantity number(4,0)  NOT NULL,
+    CONSTRAINT CompleteSet_Brick_pk PRIMARY KEY (CompleteSet_id,Brick_id)
+) ;
+
+-- Table: Corporation
+CREATE TABLE Corporation (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    CONSTRAINT Corporation_pk PRIMARY KEY (id)
+) ;
+
+-- Table: DiscontinueDate
+CREATE TABLE DiscontinueDate (
+    id number(4,0)  NOT NULL,
+    MarketEntry date  NOT NULL,
+    ExitDate date  NOT NULL,
+    CONSTRAINT DiscontinueDate_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Manual
+CREATE TABLE Manual (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    PageCount number(4,0)  NOT NULL,
+    CompleteSet_id number(4,0)  NOT NULL,
+    CONSTRAINT Manual_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Material
+CREATE TABLE Material (
+    id number(4,0)  NOT NULL,
+    name varchar2(40)  NOT NULL,
+    CONSTRAINT Material_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Minifigure
+CREATE TABLE Minifigure (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    Brick_id number(4,0)  NOT NULL,
+    Brick_2_id number(4,0)  NOT NULL,
+    Brick_3_id number(4,0)  NOT NULL,
+    Brick_4_id number(4,0)  NOT NULL,
+    CONSTRAINT Minifigure_pk PRIMARY KEY (id)
+) ;
+
+-- Table: Set_Minifigure
+CREATE TABLE Set_Minifigure (
+    CompleteSet_id number(4,0)   NULL,
+    Minifigure_id number(4,0)  NOT NULL,
+    quantity number(4,0)  NOT NULL,
+    CONSTRAINT Set_Minifigure_pk PRIMARY KEY (CompleteSet_id,Minifigure_id)
+) ;
+
+-- Table: Theme
+CREATE TABLE Theme (
+    id number(4,0)  NOT NULL,
+    Name varchar2(40)  NOT NULL,
+    Corporation_id number(4,0)  NOT NULL,
+    CONSTRAINT Theme_pk PRIMARY KEY (id)
+) ;
+
+-- foreign keys
+-- Reference: Author_CompleteSet_Author (table: Author_CompleteSet)
+ALTER TABLE Author_CompleteSet ADD CONSTRAINT Author_CompleteSet_Author
+    FOREIGN KEY (Author_id)
+    REFERENCES Author (id);
+
+-- Reference: Author_CompleteSet_CompleteSet (table: Author_CompleteSet)
+ALTER TABLE Author_CompleteSet ADD CONSTRAINT Author_CompleteSet_CompleteSet
+    FOREIGN KEY (CompleteSet_id)
+    REFERENCES CompleteSet (id);
+
+-- Reference: Box_CompleteSet (table: Box)
+ALTER TABLE Box ADD CONSTRAINT Box_CompleteSet
+    FOREIGN KEY (CompleteSet_id)
+    REFERENCES CompleteSet (id);
+
+-- Reference: Brick_Color_Brick (table: Brick_Color)
+ALTER TABLE Brick_Color ADD CONSTRAINT Brick_Color_Brick
+    FOREIGN KEY (Brick_id)
+    REFERENCES Brick (id);
+
+-- Reference: Brick_Color_Color (table: Brick_Color)
+ALTER TABLE Brick_Color ADD CONSTRAINT Brick_Color_Color
+    FOREIGN KEY (Color_id)
+    REFERENCES Color (id);
+
+-- Reference: Brick_Material (table: Brick)
+ALTER TABLE Brick ADD CONSTRAINT Brick_Material
+    FOREIGN KEY (Material_id)
+    REFERENCES Material (id);
+
+-- Reference: CompleteSet_Brick_Brick (table: CompleteSet_Brick)
+ALTER TABLE CompleteSet_Brick ADD CONSTRAINT CompleteSet_Brick_Brick
+    FOREIGN KEY (Brick_id)
+    REFERENCES Brick (id);
+
+-- Reference: CompleteSet_Brick_CompleteSet (table: CompleteSet_Brick)
+ALTER TABLE CompleteSet_Brick ADD CONSTRAINT CompleteSet_Brick_CompleteSet
+    FOREIGN KEY (CompleteSet_id)
+    REFERENCES CompleteSet (id);
+
+-- Reference: CompleteSet_BuildingDifficulty (table: CompleteSet)
+ALTER TABLE CompleteSet ADD CONSTRAINT CompleteSet_BuildingDifficulty
+    FOREIGN KEY (BuildingDifficulty_id)
+    REFERENCES BuildingDifficulty (id);
+
+-- Reference: CompleteSet_DiscontinueDate (table: CompleteSet)
+ALTER TABLE CompleteSet ADD CONSTRAINT CompleteSet_DiscontinueDate
+    FOREIGN KEY (DiscontinueDate_id)
+    REFERENCES DiscontinueDate (id);
+
+-- Reference: CompleteSet_Theme (table: CompleteSet)
+ALTER TABLE CompleteSet ADD CONSTRAINT CompleteSet_Theme
+    FOREIGN KEY (Theme_id)
+    REFERENCES Theme (id);
+
+-- Reference: Manual_CompleteSet (table: Manual)
+ALTER TABLE Manual ADD CONSTRAINT Manual_CompleteSet
+    FOREIGN KEY (CompleteSet_id)
+    REFERENCES CompleteSet (id);
+
+-- Reference: Minifigure_Brick (table: Minifigure)
+ALTER TABLE Minifigure ADD CONSTRAINT Minifigure_Brick
+    FOREIGN KEY (Brick_id)
+    REFERENCES Brick (id);
+
+-- Reference: Minifigure_Brick2 (table: Minifigure)
+ALTER TABLE Minifigure ADD CONSTRAINT Minifigure_Brick2
+    FOREIGN KEY (Brick_2_id)
+    REFERENCES Brick (id);
+
+-- Reference: Minifigure_Brick3 (table: Minifigure)
+ALTER TABLE Minifigure ADD CONSTRAINT Minifigure_Brick3
+    FOREIGN KEY (Brick_3_id)
+    REFERENCES Brick (id);
+
+-- Reference: Minifigure_Brick4 (table: Minifigure)
+ALTER TABLE Minifigure ADD CONSTRAINT Minifigure_Brick4
+    FOREIGN KEY (Brick_4_id)
+    REFERENCES Brick (id);
+
+-- Reference: Set_Minifigure_CompleteSet (table: Set_Minifigure)
+ALTER TABLE Set_Minifigure ADD CONSTRAINT Set_Minifigure_CompleteSet
+    FOREIGN KEY (CompleteSet_id)
+    REFERENCES CompleteSet (id);
+
+-- Reference: Set_Minifigure_Minifigure (table: Set_Minifigure)
+ALTER TABLE Set_Minifigure ADD CONSTRAINT Set_Minifigure_Minifigure
+    FOREIGN KEY (Minifigure_id)
+    REFERENCES Minifigure (id);
+
+-- Reference: Theme_Corporation (table: Theme)
+ALTER TABLE Theme ADD CONSTRAINT Theme_Corporation
+    FOREIGN KEY (Corporation_id)
+    REFERENCES Corporation (id);
+
+-- End of file.
+
---
+++ LegoFactory_dropMS.sql
@@ -0,0 +1,77 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2023-01-11 18:44:52.11
+
+-- foreign keys
+ALTER TABLE Author_CompleteSet DROP CONSTRAINT Author_CompleteSet_Author;
+
+ALTER TABLE Author_CompleteSet DROP CONSTRAINT Author_CompleteSet_CompleteSet;
+
+ALTER TABLE Box DROP CONSTRAINT Box_CompleteSet;
+
+ALTER TABLE Brick_Color DROP CONSTRAINT Brick_Color_Brick;
+
+ALTER TABLE Brick_Color DROP CONSTRAINT Brick_Color_Color;
+
+ALTER TABLE Brick DROP CONSTRAINT Brick_Material;
+
+ALTER TABLE CompleteSet_Brick DROP CONSTRAINT CompleteSet_Brick_Brick;
+
+ALTER TABLE CompleteSet_Brick DROP CONSTRAINT CompleteSet_Brick_CompleteSet;
+
+ALTER TABLE CompleteSet DROP CONSTRAINT CompleteSet_BuildingDifficulty;
+
+ALTER TABLE CompleteSet DROP CONSTRAINT CompleteSet_DiscontinueDate;
+
+ALTER TABLE CompleteSet DROP CONSTRAINT CompleteSet_Theme;
+
+ALTER TABLE Manual DROP CONSTRAINT Manual_CompleteSet;
+
+ALTER TABLE Minifigure DROP CONSTRAINT Minifigure_Brick;
+
+ALTER TABLE Minifigure DROP CONSTRAINT Minifigure_Brick2;
+
+ALTER TABLE Minifigure DROP CONSTRAINT Minifigure_Brick3;
+
+ALTER TABLE Minifigure DROP CONSTRAINT Minifigure_Brick4;
+
+ALTER TABLE Set_Minifigure DROP CONSTRAINT Set_Minifigure_CompleteSet;
+
+ALTER TABLE Set_Minifigure DROP CONSTRAINT Set_Minifigure_Minifigure;
+
+ALTER TABLE Theme DROP CONSTRAINT Theme_Corporation;
+
+-- tables
+DROP TABLE Author;
+
+DROP TABLE Author_CompleteSet;
+
+DROP TABLE Box;
+
+DROP TABLE Brick;
+
+DROP TABLE Brick_Color;
+
+DROP TABLE BuildingDifficulty;
+
+DROP TABLE Color;
+
+DROP TABLE CompleteSet;
+
+DROP TABLE CompleteSet_Brick;
+
+DROP TABLE Corporation;
+
+DROP TABLE DiscontinueDate;
+
+DROP TABLE Manual;
+
+DROP TABLE Material;
+
+DROP TABLE Minifigure;
+
+DROP TABLE Set_Minifigure;
+
+DROP TABLE Theme;
+
+-- End of file.
+
